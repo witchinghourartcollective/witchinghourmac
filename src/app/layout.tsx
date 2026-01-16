43925b0f58e6863e2c751a7d8247bffb47640056
Witching Hour Art Collective <thewitchinghourartcollective@gmail.com>
2026-01-16 04:06:26 -0500

    Add Google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XZYG0YRGV5"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XZYG0YRGV5');
+            `,
+          }}
+        />
+      </head>
       <body>
         <Header />
         {children}
