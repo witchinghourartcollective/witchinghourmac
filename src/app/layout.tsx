b05e7d91e5b0367505577451a45b854a7ffac870
Witching Hour Art Collective <thewitchinghourartcollective@gmail.com>
2026-01-16 03:12:17 -0500

    Remove debug colors and add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Header from "../components/Header";
 
 export const metadata = {
   title: "Witching Hour Music",
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
