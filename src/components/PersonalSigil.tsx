d211612278cdf534eb5b87f3fa68f8128ec0ef19
Witching Hour Art Collective <thewitchinghourartcollective@gmail.com>
2026-01-15 22:53:12 -0500

    Finalize sigils, styles, and site assets

---
+++ src/components/PersonalSigil.tsx
@@ -0,0 +1,13 @@
+export default function PersonalSigil() {
+  return (
+    <pre className="personal-sigil">
+{`\\    |    /
+ \\   |   /
+   \\  |  /
+     \\|/
+      /|\\
+    /  |  \\
+   /   |   \\`}
+    </pre>
+  );
+}
