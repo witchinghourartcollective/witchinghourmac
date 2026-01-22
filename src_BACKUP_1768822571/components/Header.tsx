8c6601d25ec778f1ef03a4d6747635ba4f260a93
Witching Hour Art Collective <thewitchinghourartcollective@gmail.com>
2026-01-21 22:32:00 -0500

    Update

---
+++ src_BACKUP_1768822571/components/Header.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="whm-header">
+      <Link href="/" className="sigil-lock">
+        <Image
+          src="/brand/sigil-primary.svg"
+          alt="Primary Sigil"
+          width={28}
+          height={28}
+          className="primary-sigil-mark"
+        />
+      </Link>
+
+      <nav>
+        <Link href="/about">About</Link>
+        <Link href="/ritual">Ritual</Link>
+        <Link href="/calendar">Calendar</Link>
+        <Link href="/links">Links</Link>
+      </nav>
+    </header>
+  );
+}
